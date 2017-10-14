5b0c7647cfa4633dbf5c7dbe3e262de3a0544fab
IceIceRabbit <pranav.kamojjhala@students.iiit.ac.in>
2017-10-14 10:39:42 +0000

    added environment and agent

---
+++ motion_model_reward.py
@@ -0,0 +1,70 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import numpy.linalg as la
+
+#period of testing in seconds
+time = 10
+goal = np.array([2,200])
+
+
+x1 = []
+y1 = []
+x2 = []
+y2 = []
+
+#initial positions of both enities
+x1_in = 2
+y1_in = 0
+x2_in = 2
+y2_in = 20
+
+r1 = 1
+r2 = 0.5
+epsilon = 0
+v1 = 1
+v2 = 0 # assuming a static obstacle
+a1 = 0 # constant velocity for both enities
+a2 = 0
+theta1 = math.pi/2 #assuming straight line trajectory
+theta2 = math.pi/2
+
+action_space = [-math.pi/4,-math.pi/6,0,math.pi/6,math.pi/4] #set of possible action defined for [FOR TESTING]
+
+def distance_from_collision(A1,A2):
+    la.norem(A1 - A2)
+def col_reward(X1,X2,Y1,Y2): #old reward function
+    reward = (1/(la.norm(np.array([X1,Y1]) - np.array([X2,Y2])))) * (math.pow((X1 - X2),2) + math.pow((X1 - X2),2) - math.pow((r1 + r2 + epsilon),2))
+    return reward
+
+def distance_to_goal(x1,y1):
+    dist = la.norm(np.array([x1,y1]) - goal)
+    return dist
+
+def reward(x1,y1,dmin):
+    if(dmin < 0):
+        reward = -0.25
+    elif(dmin < 2):
+        reward = -0.1 -0.5*dmin
+    elif(distance_to_goal(x1,y1) == 0):
+        reward = 1
+    else:
+        reward = 0.01
+
+    return reward
+
+def step(action,timestep):
+    x1.append(x1_in + ((v1/10)*timestep + a1*(math.pow(timestep,2)))*math.cos(action_space[action])) #agent movement according action provided
+    y1.append(y1_in + ((v1/10)*timestep + a1*(math.pow(timestep,2)))*math.sin(action_space[action]))
+    x2.append(x2_in + ((v2/10)*timestep + a2*(math.pow(timestep,2)))*math.cos(theta2))
+    y2.append(y2_in + ((v2/10)*timestep + a2*(math.pow(timestep,2)))*math.sin(theta2))
+    timestep += 1
+    done = False
+    if distance_to_goal(x1[timestep-1],y1[timestep-1]) == 0:
+        done = True
+    return [x1[timestep-1],y1[timestep - 1]] , reward( x1[timestep-1] , y1[timestep-1] , distance_from_collision(np.array([x1[timestep-1],y1[timestep-1]]),np.array([x2[timestep-1],y2[timestep-1]]))), done,timestep
+
+def plot():
+    plt.plot(x1,y1)
+    plt.plot(x2,y2,'ro')
+    plt.show()
---
+++ q_learning_agent.py
@@ -0,0 +1,59 @@
+import numpy as np
+import random
+from collections import defaultdict
+import motion_model_reward as mmr
+
+
+class QLearningAgent:
+    def __init__(self, actions):
+        self.actions = actions
+        self.learning_rate = 0.01
+        self.discount_factor = 0.9
+        self.epsilon = 0.1
+        self.q_table =  defaultdict(lambda: [0])
+
+    def learn(self, state, action, reward, next_state):
+        current_q = self.q_table[state][action]
+        new_q = reward + self.discount_factor * max(self.q_table[next_state])
+        self.q_table[state][action] += self.learning_rate * (new_q - current_q)
+
+    def get_action(self, state):
+        if np.random.rand() < self.epsilon:
+            action = np.random.choice(self.actions)
+        else:
+            state_action = self.q_table[state]
+            action = self.arg_max(state_action)
+        return action
+
+    @staticmethod
+    def arg_max(state_action):
+        max_index_list = []
+        max_value = state_action[0]
+        for index, value in enumerate(state_action):
+            if value > max_value:
+                max_index_list.clear()
+                max_value = value
+                max_index_list.append(index)
+            elif value == max_value:
+                max_index_list.append(index)
+        return random.choice(max_index_list)
+
+
+
+if __name__ == "__main__":
+    agent = QLearningAgent(actions = list(range(5)))
+
+    for episode in range(1000):
+        state = [mmr.x1_in,mmr.y1_in]
+        timestep = 1
+        while True:
+
+            action = agent.get_action(str(state))
+            next_state,reward ,done,timestep = mmr.step(action,timestep)
+            agent.learn(str(state), action, reward, str(next_state)) #reward defined by the sum of heuristics
+
+            state = next_state
+
+            if done:
+                break
+        mmr.plot()
